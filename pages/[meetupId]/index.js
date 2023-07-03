60cc2bc4648d7a2ece8cebba9e38c367adf2a022
mre55 <misganawmihreteab>
2023-07-03 15:35:12 +0300

    Add getStaticProps to meetup detail page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,21 @@ const MeetupDetails = () => {
   );
 };
 
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "First meetup",
+        address: "Some Street 5, Some City",
+        description: "This is a first meetup",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
