b5c09360c372a93d51785cccf75e3a13c0210bb2
mre55 <misganawmihreteab>
2023-07-03 16:13:12 +0300

    Add mongodb connection to db

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,12 +1,26 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup
 
-function handler(req, res) {
-  if(req.method === 'POST') {
+async function handler(req, res) {
+  if (req.method === "POST") {
     const data = req.body;
 
-    const {title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://javascriptmastery:oOUxsovcAXvJRzmU@cluster0.mfetfx7.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
 
-    
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne({ data });
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
