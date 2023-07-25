69b272f37ab35bedcd49dce2396dbe62636ada53
mre55 <misganawmihreteab>
2023-07-25 13:49:53 +0300

    Edit meetupid file

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,15 +1,15 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const MeetupDetails = () => {
+function MeetupDetails() {
   return (
     <MeetupDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
-      title="First meetup"
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+      title="First Meetup"
       address="Some Street 5, Some City"
       description="This is a first meetup"
     />
   );
-};
+}
 
 export async function getStaticPaths() {
   return {
@@ -30,15 +30,19 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
   const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
 
   return {
     props: {
       meetupData: {
         image:
-          "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
         id: meetupId,
-        title: "First meetup",
+        title: "First Meetup",
         address: "Some Street 5, Some City",
         description: "This is a first meetup",
       },
